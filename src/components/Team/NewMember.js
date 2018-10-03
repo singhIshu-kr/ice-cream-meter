e8034466dcfb2dd8ade6747daac6c94b57e7740e
Ishu Singh <ishusi@In-Ishusi.local>
2018-10-03 20:06:18 +0530

    Modify the error message

--- src/components/Team/NewMember.js
+++ src/components/Team/NewMember.js
@@ -40,7 +40,7 @@ class NewMember extends React.Component {
     const { nameInUse } = this.props;
     return (
       <div className="addMember">
-        {nameInUse && <p className="invalid-credentials">Id should be unique</p>}
+        {nameInUse && <p className="invalid-credentials">Name should be unique</p>}
         <label>
           <input type="text" id="name" placeholder="Name" value={this.state.value} onChange={this.handleChange} onKeyPress={(event)=>this.submitForm(event)} />
           <input id="submit" type="submit" className="primary" value="Add Member" onClick={this.handleSubmit} />

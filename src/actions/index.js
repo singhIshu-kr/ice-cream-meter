a5f7096b66ff54508fb3118e328218d3837e8d0c
Ishu Singh <ishusi@In-Ishusi.local>
2018-10-21 18:04:56 +0530

    Add route of "team/teamName"

--- src/actions/index.js
+++ src/actions/index.js
@@ -133,6 +133,7 @@ export const addNewTeam = (dispatch, userId, teamName) => {
   let body = { userId, teamName}
   return axios.post('/newTeam', body, { headers: { accesstoken: cookie.load('accessToken'), email: cookie.load('email') } }).then((res) => {
     if(res.status === 200){
+      cookie.save("teamId", teamName, { path: "/" });
       return dispatch({
         type: "TEAM_CREATED"
       })
@@ -142,6 +143,7 @@ export const addNewTeam = (dispatch, userId, teamName) => {
 
 export const getTeamsOfUser = (dispatch, userId) =>{
   return axios.get(`/myTeams/${userId}`, { headers: { accesstoken: cookie.load('accessToken'), email: cookie.load('email') } }).then((res) => {
+    console.log(res)
     if(res.status === 200){
       return dispatch({
         type: "GET_TEAMS_OF_USER",
--- src/components/User/Container.js
+++ src/components/User/Container.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import NewTeam from './NewTeam'
-
+import { Redirect, Link } from 'react-router-dom';
+import cookie from 'react-cookies';
 
 class Container extends React.Component {
   componentWillMount(){
-    const { getTeamsOfUser, userId,teams} = this.props;
+    const { getTeamsOfUser, userId} = this.props;
     getTeamsOfUser(userId);
   }
 
   render(){
-    const {activeElement, addNewTeam, userId, teams} = this.props;
+    const {activeElement, addNewTeam, userId, teams,teamCreated} = this.props;
+    if(teamCreated){
+      return <Redirect to={{ pathname: `/team/${cookie.load("teamId")}` }} />
+    }
     return(
       <div>
         <NewTeam activeElement={activeElement} addNewTeam={addNewTeam} userId={userId} />
         {
           teams.map((team)=>{
             return (
-              <h3>{team.teamId}</h3>
+              <p><Link to={`/team/${team.teamId}`}>{team.teamId}</Link></p>
             )
           })
         }

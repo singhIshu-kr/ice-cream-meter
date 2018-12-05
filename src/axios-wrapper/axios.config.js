7268246c4fd1230463f70dff159b2b7332d160f8
Ishu Singh <ishusi@In-Ishusi.local>
2018-12-05 21:41:03 +0530

    change the service url from local to the deployed version

--- src/axios-wrapper/axios.config.js
+++ src/axios-wrapper/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL:'http://localhost:8008'
-  // baseURL: 'https://ice-cream-service.herokuapp.com/'
+  // baseURL:'http://localhost:8008'
+  baseURL: 'https://ice-cream-service.herokuapp.com/'
 });

b8eb06857b6a839f5ea22e600dedfcd61a5153b1
Ishusi <ishusi@IN-Ishusi.local>
2019-03-10 18:08:15 +0530

    Change the service url to heroku

--- src/axios-wrapper/axios.config.js
+++ src/axios-wrapper/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL:'http://localhost:80'
-  // baseURL: 'https://ice-cream-service.herokuapp.com/'
+  // baseURL:'http://localhost:8000'
+  baseURL: 'https://ice-cream-service.herokuapp.com/'
 });

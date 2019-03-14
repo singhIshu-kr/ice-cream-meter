6d5ae42dd1eaf137ac620da3e68b9e92bb15cf3a
Ishusi <ishusi@IN-Ishusi.local>
2019-03-14 18:08:47 +0530

    Dummy commit to check the integration with heroku

--- src/axios-wrapper/axios.config.js
+++ src/axios-wrapper/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  // baseURL:'http://localhost:8000'
+  // baseURL:'http://localhost:8000'  Dummy commit
   baseURL: 'https://ice-cream-service.herokuapp.com/'
 });

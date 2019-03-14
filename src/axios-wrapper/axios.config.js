9e8726aded4588eebf972de4bd159571b219fff8
Ishusi <ishusi@IN-Ishusi.local>
2019-03-14 18:12:36 +0530

    Dummy commit to check the automatic deployment on heroku is happening

--- src/axios-wrapper/axios.config.js
+++ src/axios-wrapper/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  // baseURL:'http://localhost:8000'  Dummy commit
+  // baseURL:'http://localhost:8000'  Dummy commit 2
   baseURL: 'https://ice-cream-service.herokuapp.com/'
 });
